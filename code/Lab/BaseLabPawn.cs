86fcabbc6088d58b89259cefdeec6731a7e38d51
Garry Newman <garrynewman@users.noreply.github.com>
2021-08-26 11:55:35 +0100

    Initial

---
+++ code/Lab/BaseLabPawn.cs
@@ -0,0 +1,59 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace Lab
+{
+	public partial class BaseLabPawn : Sandbox.AnimEntity
+	{
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			Tags.Add( "player", "pawn" );
+			SetModel( "models/light_arrow.vmdl" );
+		}
+
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+
+			Rotation = Input.Rotation;
+			EyeRot = Rotation;
+
+			DoMovement( cl );
+		}
+
+		public virtual void DoMovement( Client cl )
+		{
+			var maxSpeed = 500;
+			if ( Input.Down( InputButton.Run ) ) maxSpeed = 1000;
+			if ( Input.Down( InputButton.Duck ) ) maxSpeed = 100;
+
+			Velocity = Velocity.AddClamped( Input.Rotation * new Vector3( Input.Forward, Input.Left, Input.Up ) * maxSpeed * 5 * Time.Delta, maxSpeed );
+			Velocity = Velocity.Approach( 0, Time.Delta * maxSpeed * 3 );
+
+			//
+			// Move helper traces and slides along surfaces for us
+			//
+			MoveHelper helper = new MoveHelper( Position, Velocity );
+			helper.Trace = helper.Trace.Size( 20 );
+
+			helper.TryUnstuck();
+			helper.TryMoveWithStep( Time.Delta, 30.0f );
+
+			Position = helper.Position;
+			Velocity = helper.Velocity;
+			EyePos = Position;
+		}
+
+		public override void FrameSimulate( Client cl )
+		{
+			base.FrameSimulate( cl );
+
+			Rotation = Input.Rotation;
+			EyeRot = Rotation;
+			Position += Velocity * Time.Delta;
+		}
+	}
+
+}
---
+++ code/LabPawn.cs
@@ -0,0 +1,48 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab
+{
+	public partial class LabPawn : BaseLabPawn
+	{
+		[ConVar.ClientData( "lab_toolmode" )]
+		public string ToolMode { get; set; } = "create";
+
+
+		[Net]
+		public List<Entity> Selected { get; set; }
+
+		public override void Simulate( Client cl )
+		{
+			base.Simulate( cl );
+
+
+			if ( !Input.VR.IsActive ) return;
+
+			DebugDrawHand( "Left", Input.VR.LeftHand );
+			DebugDrawHand( "Right", Input.VR.RightHand );
+
+			foreach ( var obj in Input.VR.TrackedObjects )
+			{
+				DebugDrawTracked( obj );
+			}
+		}
+
+		private void DebugDrawTracked( Input.TrackedObject obj )
+		{
+			DebugOverlay.Box( obj.Transform.Position, obj.Transform.Rotation, -0.9f, 0.9f, Color.White, 0.0f, true );
+			DebugOverlay.Text( obj.Transform.Position, $"Type: {obj.Type}", 0.0f );
+		}
+
+		private void DebugDrawHand( string v, Input.VrHand hand )
+		{
+			DebugOverlay.Box( hand.Transform.Position, hand.Transform.Rotation, -1, 1, Color.Blue, 0.0f, true );
+			DebugOverlay.Text( hand.Transform.Position, v, 0.0f );
+		}
+	}
+
+}
